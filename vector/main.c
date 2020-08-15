a80ffc113ac5b184c980671129ab1960945a9ce4
Gurankio <gurankio@icloud.com>
2020-08-15 22:43:10 +0200

    Initial Commit

---
+++ vector/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  vector
+//
+//  Created by Gurankio on 15/08/2020.
+//  Copyright © 2020 Gurankio. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
